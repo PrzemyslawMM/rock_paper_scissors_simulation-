f7c81b45cbb5addf7a87c418404c81e93419f5ad
PrzemyslawMM <vertezelopl@gmail.com>
2025-07-05 21:07:26 +0200

    Changed how data is being showned

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-# import matplotlib.pyplot as plt
-# import random
-
-
-# symulation = Simulation_Chamber()
-
-# k = 1000
-# for i in range(k):
-#   symulation.simulation_day()
-
-# x = [i for i in range(0, k+1)]
-# plt.plot(x, symulation.rock, c="grey", linewidth=1, label="ROCK")
-# plt.plot(x, symulation.paper, c="blue", linewidth=1, label="PAPER")
-# plt.plot(x, symulation.scissors, c="black", linewidth=1, label="SCISSORS")
-# plt.legend(bbox_to_anchor=(1,1))
-# plt.show()
-
 from Simulation_Chamber import Simulation_Chamber
+import matplotlib.pyplot as plt
+
 simulation = Simulation_Chamber()
 
-k = 100
+k = 1000
 for i in range(k):
   simulation.simulation_day()
 
-print(simulation.rock) 
-print(simulation.paper) 
-print(simulation.scissors)
+x = [i for i in range(0, k+1)]
+plt.plot(x, simulation.rock, c="grey", linewidth=1, label="ROCK")
+plt.plot(x, simulation.paper, c="blue", linewidth=1, label="PAPER")
+plt.plot(x, simulation.scissors, c="black", linewidth=1, label="SCISSORS")
+plt.legend(bbox_to_anchor=(1,1))
+plt.show()
+
+# simulation = Simulation_Chamber()
+
+# k = 100
+# for i in range(k):
+#   simulation.simulation_day()
+
+# print(simulation.rock) 
+# print(simulation.paper) 
+# print(simulation.scissors)
